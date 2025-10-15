44e854fa6d80217d6eaa46e5bc8c3fe837d5e05e
oblivjules <marvelingjules@gmail.com>
2025-10-15 16:22:23 +0800

    feat: add dashboard and requests module with project restructuring

--- docutrackr/urls.py
+++ docutrackr/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('authentication.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('requests/', include('requests.urls')),
 ]
