b797365d083dda3c10d28380d50a292ef0c18617
oblivjules <marvelingjules@gmail.com>
2025-11-09 23:15:32 +0800

    fix: Add STATIC_ROOT setting to Django settings for static file management

--- docutrackr/settings.py
+++ docutrackr/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 
 DEBUG = True
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
